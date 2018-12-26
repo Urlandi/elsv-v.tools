30b1b4b130240c4742d0a9d76be876ef85e407b9
Mikhail Vasilyev <letitbee@lesv-v.ru>
2018-12-26 18:41:55 +0400

    Fetch all ASn's links

--- dotlinks.py
+++ dotlinks.py
@@ -30,6 +30,20 @@ Input file (or STDIN) format is an ASn in each line
 SUCCESS = 0
 ERR_IO = 2
 ERR_GETOPT = 3
+ERR_GETASN = 4
+
+
+def get_whois_asn_list(import_records):
+
+    def lambda_get_peerases(asn_list, record):
+        if asn_list is None:
+            return None
+        elif rpsl.RE_ASSET_ANY in asn_list:
+            return {rpsl.RE_ASSET_ANY}
+
+        return asn_list.union(rpsl.get_peerases(record))
+
+    return reduce(lambda_get_peerases, import_records, set())
 
 
 def main(opt_all=False):
@@ -51,33 +65,58 @@ def main(opt_all=False):
         if len(args) > 0:
             input_flow_name = args[-1]
 
+        asn_links = dict()
+
         for line in fileinput.input(input_flow_name):
 
-            rline = line.strip()
-            if not re.match(rpsl.RE_ASN, rline, re.IGNORECASE):
+            asn = line.strip()
+            if not re.match(rpsl.RE_ASN, asn, re.IGNORECASE):
                 continue
 
-            whois_asn = ripeapi.get_whois_top(rline)
+            whois_asn = ripeapi.get_whois_top(asn)
 
             if whois_asn is not None:
+                asn_links[asn] = dict()
 
-                def get_whois_asn_list(_whois_asn_list, record_type):
-                    if _whois_asn_list is None:
-                        return None
-                    elif rpsl.RE_ASSET_ANY in _whois_asn_list:
-                        return {rpsl.RE_ASSET_ANY}
+                for record_type in ("import", "export", "default", "mp-import", "mp-export", "mp-default",):
 
-                    import_types = ("import", "export", "default", "mp-import", "mp-export", "mp-default",)
+                    if record_type in whois_asn:
+                        asn_list = get_whois_asn_list(whois_asn[record_type])
+                    else:
+                        asn_list = set()
 
-                    if record_type in import_types:
-                        pass
+                    if asn_list is None:
+                        err_id = ERR_GETASN
+                        break
+                    else:
+                        if record_type == "default":
+                            asn_links[asn]["import"].update(asn_list)
+                        elif record_type == "mp-default":
+                            asn_links[asn]["mp-import"].update(asn_list)
+                        else:
+                            asn_links[asn][record_type] = set()
+                            asn_links[asn][record_type].update(asn_list)
 
-                    return _whois_asn_list.union()
+                if err_id != SUCCESS:
+                    break
 
-                whois_asn_list = set(reduce(get_whois_asn_list, whois_asn, set()))
-                print(whois_asn_list)
+                peers = ripeapi.get_neighbours(asn)
+                if peers is not None:
+                    asn_links[asn]["uplinks"] = peers["left"]
+                    asn_links[asn]["downlinks"] = peers["right"]
+                    asn_links[asn]["peers"] = peers["uncertain"]
+                else:
+                    err_id = ERR_GETASN
+                    break
+
             else:
-                continue
+                err_id = ERR_GETASN
+                break
+
+        if err_id != SUCCESS:
+            print ("Break because is fatal error then get links via RIPE API")
+        else:
+            print(asn_links)
 
     except IOError:
         print ("Input read error in '{}'".format(input_flow_name))
